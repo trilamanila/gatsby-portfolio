01af376be2409979a35697562d940c446caf654f
JC <jctrilamanila@gmail.com>
2021-05-29 23:58:56 -0400

    updated portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: 'Hi, my name is Joncin Picol based in Delaware.',
   paragraphTwo: 'I am a father of two and coding is a passion of mine. ',
   paragraphThree:
-    'I enjoy creating modern visually pleasing websites and have been dedicating my time replicating the most popular websites to stay on the cutting edge of frontend developement. My goal is push Bitocin adoption to the mass with beautiful front end designs.',
+    'I enjoy creating modern visually pleasing websites and have been dedicating my time replicating the most popular websites to stay on the cutting edge of frontend developement. My goal is to be able to onboard more people to technology with easy to use front end designs to better daily life.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
